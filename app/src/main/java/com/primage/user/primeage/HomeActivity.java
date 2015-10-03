86cc26882c4d64a1c253b64286fe5033b15eb76d
eilonKarty <eilon.karty@mail.huji.ac.il>
2015-10-03 17:12:44 +0300

    small changes

--- app/src/main/java/com/primage/user/primeage/HomeActivity.java
+++ app/src/main/java/com/primage/user/primeage/HomeActivity.java
@@ -1,14 +1,9 @@
 package com.primage.user.primeage;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-
 
 
 public class HomeActivity extends Activity {
@@ -23,15 +18,4 @@ public class HomeActivity extends Activity {
         Intent i = new Intent(this, AppsListActivity.class);
         startActivity(i);
     }
-
-    public static class PButton extends Button {
-        public PButton(Context context, AttributeSet attrs) {
-            super(context, attrs);
-            System.out.print("THIS IS MY MF CONSTRUCTOR!!!");
-            Log.d("THIS IS ALSO", "MY MF LOG");
-            //TODO - STUFF
-        }
-    }
-
-
 }
