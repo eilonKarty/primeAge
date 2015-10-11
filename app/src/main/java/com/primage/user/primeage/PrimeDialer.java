6a001a88a8667d506b4ad870e13dc93b125e1c01
eilonKarty <eilon.karty@mail.huji.ac.il>
2015-10-11 03:14:08 +0300

    dialer

--- app/src/main/java/com/primage/user/primeage/PrimeDialer.java
+++ app/src/main/java/com/primage/user/primeage/PrimeDialer.java
@@ -1,18 +1,26 @@
 package com.primage.user.primeage;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class PrimeDialer extends Activity {
 
+    private String number = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prime_dialer);
+
+        // Setting the background color to white
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(-1);
     }
 
     @Override
@@ -39,5 +47,117 @@ public class PrimeDialer extends Activity {
 
     public void doNothing(View v){
         // A void function
+    }
+
+    private void checkNumberSize(){
+        if(number.length() == 3){
+            number += "-";
+        }
+    }
+
+    public void addOne(View v){
+        number = number + "1";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addTwo(View v){
+        number = number + "2";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addThree(View v){
+        number = number + "3";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addFour(View v){
+        number = number + "4";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addFive(View v){
+        number = number + "5";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addSix(View v){
+        number = number + "6";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addSeven(View v){
+        number = number + "7";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addEight(View v){
+        number = number + "8";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addNine(View v){
+        number = number + "9";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addZero(View v){
+        number = number + "0";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addHash(View v){
+        number = number + "#";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void addStar(View v){
+        number = number + "0";
+        TextView tv = (TextView)findViewById(R.id.number);
+        tv.setText(number);
+        checkNumberSize();
+    }
+
+    public void deleteFigure(View v) {
+        if (number.length() != 0){
+            if (number.length() == 5) {
+                number = number.substring(0, number.length() - 2);
+            } else {
+                number = number.substring(0, number.length() - 1);
+            }
+
+            TextView tv = (TextView) findViewById(R.id.number);
+            tv.setText(number);
+        }
+    }
+
+    public void makeCall(View v){
+        if(number.length() !=0) {
+            String str = "tel:" + number;
+            Uri uri = Uri.parse(str);
+            Intent intent = new Intent(Intent.ACTION_CALL, uri);
+            startActivity(intent);
+        }
     }
 }
