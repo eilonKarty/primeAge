1bf15f20deb2a02ec0abfe418108bde492472c77
Tzion <tzion12345@gmail.com>
2015-10-25 04:30:52 +0200

    before fixing TTS

--- app/src/main/java/com/primage/user/primeage/PrimeDialer.java
+++ app/src/main/java/com/primage/user/primeage/PrimeDialer.java
@@ -19,7 +19,6 @@ import android.widget.TextView;
 public class PrimeDialer extends Activity {
     boolean isAssistantMode = false;
     private Assistant assistant;
-
     private String number = "";
 
     @Override
@@ -32,207 +31,8 @@ public class PrimeDialer extends Activity {
         // Setting the background color to white
         View view = this.getWindow().getDecorView();
         view.setBackgroundColor(-1);
-        final Context context = getApplicationContext();
 
-        Button one = (Button) findViewById(R.id.oneButton);
-        one.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (SimpliSystemServices.waitAfterClick(v, event, context)) {
-                    addFigure('1');
-                }
-                ;
-                return false;
-            }
-        });
-
-        Button two = (Button) findViewById(R.id.twoButton);
-        two.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('2');
-                };
-                return false;
-            }
-        });
-
-        Button three = (Button) findViewById(R.id.threeButton);
-        three.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('3');
-                };
-                return false;
-            }
-        });
-
-        Button four = (Button) findViewById(R.id.fourButton);
-        four.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('4');
-                };
-                return false;
-            }
-        });
-
-        Button five = (Button) findViewById(R.id.fiveButton);
-        five.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (SimpliSystemServices.waitAfterClick(v, event, context)) {
-                    addFigure('5');
-                }
-                ;
-                return false;
-            }
-        });
-
-        Button six = (Button) findViewById(R.id.sixButton);
-        six.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('6');
-                };
-                return false;
-            }
-        });
-
-        Button seven = (Button) findViewById(R.id.sevenButton);
-        seven.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('7');
-                };
-                return false;
-            }
-        });
-
-        Button eight = (Button) findViewById(R.id.eightButton);
-        eight.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('8');
-                };
-                return false;
-            }
-        });
-
-        Button nine = (Button) findViewById(R.id.nineButton);
-        nine.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('9');
-                };
-                return false;
-            }
-        });
-
-        Button star = (Button) findViewById(R.id.starButton);
-        star.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('*');
-                };
-                return false;
-            }
-        });
-
-        Button zero = (Button) findViewById(R.id.zeroButton);
-        zero.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('0');
-                };
-                return false;
-            }
-        });
-
-        Button hash = (Button) findViewById(R.id.hashButton);
-        hash.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context)){
-                    addFigure('#');
-                };
-                return false;
-            }
-        });
-
-        Button call = (Button) findViewById(R.id.callbutton);
-        call.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context))
-                {
-                    makeCall(v);
-                }
-                return false;
-            }
-        });
-
-        Button delete = (Button) findViewById(R.id.deletebutton);
-        delete.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context))
-                {
-                    deleteFigure(v);
-                }
-                return false;
-            }
-        });
-
-        Button backbutton = (Button) findViewById(R.id.dialback);
-        backbutton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context))
-                {
-                    finish();
-                }
-                return false;
-            }
-        });
-
-        Button homeButton = (Button) findViewById(R.id.dialhome);
-        homeButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(SimpliSystemServices.waitAfterClick(v, event, context))
-                {
-                    goHome(v);
-                }
-                return false;
-            }
-        });
-
-    /*
-        //Listener of assitant
-        Button assistantButton = (Button) findViewById(R.id.assistant_button);
-        assistantButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (SimpliSystemServices.waitAfterClick(v, event, context)) {
-                    if (!isAssistantMode) {
-                        setAssistantMode(v);
-                    } else {
-                        setGeneralMode(v);
-                    }
-                }
-                return false;
-            }
-        });
-      */
+        setListeners();
     }
 
     @Override
@@ -274,8 +74,17 @@ public class PrimeDialer extends Activity {
         checkNumberSize();
     }
 
+    public void finishAct(View v){
+        finish();
+    }
+
+    public void goHome(View v){
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
+    }
+
+
     public void deleteFigure(View v) {
-        if (isAssistantMode) {
             boolean flg = false;
             if (number.length() != 0) {
                 if (number.length() == 5 || number.length() == 9) {
@@ -291,15 +100,9 @@ public class PrimeDialer extends Activity {
                     number += "-";
                 }
             }
-        }
-        else {
-            assistant.button.setText("Press here in order \n  to delete a digit \n from text box");
-            assistant.speakOut("Press here in order to delete a digit from text box");
-        }
     }
 
     public void makeCall(View v){
-        if (!isAssistantMode) {
             if (number.length() != 0) {
                 String str = "tel:" + number;
                 Uri uri = Uri.parse(str);
@@ -308,21 +111,255 @@ public class PrimeDialer extends Activity {
                     startActivity(intent);
                 } catch (Exception e) {
                 }
-                ;
             }
+    }
+
+    /* SWITCH BETWEEN ASSISTANT MODE AND GENERAL MODE */
+
+    public void setGeneralMode(View view) {
+        setContentView(R.layout.activity_prime_dialer);
+        isAssistantMode = false;
+        setListeners();
+    }
+
+    public void setAssistantMode(View view) {
+        setContentView(R.layout.activity_prime_dialer_mode_assistant);
+        assistant.speakOut(" Hi again, \n welcome to the dialer, \n don't forget that we are always together");
+        assistant.button = (Button) findViewById(R.id.assistant_button);
+        assistant.button.setText(" Hi again, welcome to \n the dialer, \n don't forget that we \n are always together");
+        isAssistantMode = true;
+        setListeners();
+    }
+
+    // Set listeners
+    private void setListeners(){
+        //Listener of assitant
+        assistant.button = (Button)findViewById(R.id.assistant_button);
+        if (isAssistantMode) {
+            assistant.button.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, getApplicationContext())) {
+                        setGeneralMode(v);
+                    }
+                    return false;
+                }
+            });
         }
         else {
-            assistant.button.setText("Press here to make a \n call after inserting \n the number");
-            assistant.speakOut("Press here to make a call after inserting the number");
+            assistant.button.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, getApplicationContext())) {
+                        setAssistantMode(v);
+                    }
+                    return false;
+                }
+            });
         }
-    }
 
-    public void finishAct(View v){
-        finish();
-    }
+        if (!isAssistantMode) {
+            Button one = (Button) findViewById(R.id.oneButton);
+            one.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('1');
+                    }
+                    ;
+                    return false;
+                }
+            });
 
-    public void goHome(View v){
-        Intent i = new Intent(this, HomeActivity.class);
-        startActivity(i);
+            Button two = (Button) findViewById(R.id.twoButton);
+            two.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('2');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button three = (Button) findViewById(R.id.threeButton);
+            three.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('3');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button four = (Button) findViewById(R.id.fourButton);
+            four.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('4');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button five = (Button) findViewById(R.id.fiveButton);
+            five.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('5');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button six = (Button) findViewById(R.id.sixButton);
+            six.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('6');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button seven = (Button) findViewById(R.id.sevenButton);
+            seven.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('7');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button eight = (Button) findViewById(R.id.eightButton);
+            eight.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('8');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button nine = (Button) findViewById(R.id.nineButton);
+            nine.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('9');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button star = (Button) findViewById(R.id.starButton);
+            star.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('*');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button zero = (Button) findViewById(R.id.zeroButton);
+            zero.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('0');
+                    }
+                    ;
+                    return false;
+                }
+            });
+
+            Button hash = (Button) findViewById(R.id.hashButton);
+            hash.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        addFigure('#');
+                    }
+                    ;
+                    return false;
+                }
+            });
+        }
+
+        Button call = (Button) findViewById(R.id.callbutton);
+        call.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                    if (!isAssistantMode) {
+                        makeCall(v);
+                    }
+                    else {
+                        assistant.button.setText("Press here to make a \n call after inserting \n the number");
+                        assistant.speakOut("Press here to make a call after inserting the number");
+                    }
+                }
+                return false;
+            }
+        });
+
+        Button delete = (Button) findViewById(R.id.deletebutton);
+        delete.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                    if (!isAssistantMode) {
+                        deleteFigure(v);
+                    }
+                    else {
+                        assistant.button.setText(" Press here in order \n to delete a digit \n from text box");
+                        assistant.speakOut("Press here in order to delete a digit from text box");
+                    }
+                }
+                return false;
+            }
+        });
+
+        if (!isAssistantMode) {
+            Button backbutton = (Button) findViewById(R.id.dialback);
+            backbutton.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        finish();
+                    }
+                    return false;
+                }
+            });
+
+            Button homeButton = (Button) findViewById(R.id.dialhome);
+            homeButton.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (SimpliSystemServices.waitAfterClick(v, event, PrimeDialer.this)) {
+                        goHome(v);
+                    }
+                    return false;
+                }
+            });
+        }
     }
 }
